be65c2f284d22c6ef212f9fbc7b2c07fb8e7dd43
Juan Manuel Berros <juanmaberros@gmail.com>
2018-08-01 14:19:35 +0000

    auto backup at gondolin

--- cache/production/sql_09e8507fa4de27ff8b346bfeae85c9b8.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-1526003859
-SELECT smiley_id FROM phpbb_smilies WHERE display_on_posting = 0 LIMIT 1
-6
-a:0:{}
--- cache/production/sql_efd26735c3bcbef398e23fb85e7ccd99.php
+++ cache/production/sql_efd26735c3bcbef398e23fb85e7ccd99.php
@@ -1,5 +1,5 @@
 <?php exit; ?>
-1526004287
+1533068768
 SELECT forum_id FROM phpbb_forums WHERE forum_options & 2 <> 0 LIMIT 1
 6
 a:0:{}
